5b9c582558c44a1bf471b37b76c357e2996adb1b
Juan Manuel Ramos Ortiz <43794246+JuanManuel98@users.noreply.github.com>
2024-05-27 13:00:55 -0600

    Add files via upload

---
+++ Test/src/Test/Roman.java
@@ -0,0 +1,79 @@
+package Test;
+import java.util.Scanner;
+
+public class Roman {
+	//TO DO
+	//Get a String (roman number) in standard input and convert to the equivalent in decimal
+	//C -> 100 XC XCIX
+	//L -> 50 XL XLIX  
+	//X -> 10 IX XX XXX
+	//V -> 5
+	//I -> 1 IV II III
+	public static int romanToDecimal(String cadena) {
+		char[] charArray = cadena.toCharArray();
+		int decimal = 0;
+		for(int i = 0; i < cadena.length(); i++) {
+			switch (charArray[i]) {
+				case('C'):
+					decimal += 100;
+					break;
+				case ('L'):
+					decimal += 50;
+					break;
+				case ('X'):
+					if(i < cadena.length() - 1) {
+						switch(charArray[i] + 1) {
+						case ('C'):
+							decimal += 90;
+							i++;
+							break;
+						case ('L'):
+							decimal += 40;
+							i++;
+							break;
+						default :
+							decimal += 10;
+							break;
+						}
+					} else {
+						decimal += 10;
+					}
+				case ('V'):
+					decimal += 5;
+				break;
+				case ('I'):{
+					if(i < cadena.length() - 1) {
+						switch (charArray[i+1]) {
+						case ('X'):
+							decimal += 9;
+							i++;
+							break;
+						case ('V'):
+							decimal +=4;
+							i++;
+							break;
+						default:
+							decimal += 1;
+							break;
+						}
+					} else {
+						decimal +=1;
+					}
+				}
+			}
+				
+			
+		}
+		return decimal;
+			
+	}
+	
+	public static void main(String args[]) {
+		String string = "XLIV";
+		
+		
+		int decimal = romanToDecimal(string);
+		System.out.println (decimal);
+	}
+	
+}
